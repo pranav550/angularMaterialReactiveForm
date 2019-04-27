e4900267f4d1233eb8b156f5ba87d4cf9d7e2e86
pranav <pranavverma550@gmail.com>
2019-04-27 20:41:02 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {FormGroup,  FormBuilder,  NgForm, Validators} from "@angular/forms"
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactive-material';
+  regiForm:FormGroup;
+  firstname:string="";
+  lastname:string="";
+  address:string="";
+  dob:string="";
+  gender:string="";
+  course:string;
+  option:string=""
+  email:string=""
+constructor(private fb:FormBuilder){
+
+  this.regiForm= fb.group({
+    'firstname':[null, Validators.required],
+    'lastname':[null, Validators.required],
+    'address':[null, Validators.required],
+    'dob':[null, Validators.required],
+    'gender':[null, Validators.required],
+    'course':[null, Validators.required],
+    "option":[null, Validators.required],
+    "email":[null, Validators.compose([Validators.required, Validators.email])],
+     
+  })
+
+}
+
+onFormSubmit(form:NgForm){
+   console.log(this.regiForm)
+}
+
+
+
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,60 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatToolbarModule,
+  MatInputModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatButtonModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatRadioModule,
+  MatCheckboxModule,
+  MatSelectModule,
+  
 
+
+} from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+  MatInputModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatButtonModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatRadioModule,
+  MatCheckboxModule,
+  MatSelectModule,
+  MatInputModule
   ],
+  exports:[
+    MatToolbarModule,
+    MatInputModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatInputModule
+  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
